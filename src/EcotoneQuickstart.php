f2c1e52a837b432944c78026fa87a07aaa47d142
dgafka <dgafka.mail@gmail.com>
2020-01-21 23:35:12 +0100

    update ecotone

--- src/EcotoneQuickstart.php
+++ src/EcotoneQuickstart.php
@@ -5,15 +5,15 @@ namespace App;
 
 use App\Domain\Product\GetProductPriceQuery;
 use App\Domain\Product\RegisterProductCommand;
-use Ecotone\Modelling\CommandBusWithEventPublishing;
+use Ecotone\Modelling\CommandBus;
 use Ecotone\Modelling\QueryBus;
 
 class EcotoneQuickstart
 {
-    private CommandBusWithEventPublishing $commandBus;
+    private CommandBus $commandBus;
     private QueryBus $queryBus;
 
-    public function __construct(CommandBusWithEventPublishing $commandBus, QueryBus $queryBus)
+    public function __construct(CommandBus $commandBus, QueryBus $queryBus)
     {
         $this->commandBus = $commandBus;
         $this->queryBus = $queryBus;
