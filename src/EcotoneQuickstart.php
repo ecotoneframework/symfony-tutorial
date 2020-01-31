f3539595bae7a12982ec93de770e621502b81631
dgafka <dgafka.mail@gmail.com>
2020-01-31 21:11:52 +0100

    currency exchanger

--- src/EcotoneQuickstart.php
+++ src/EcotoneQuickstart.php
@@ -19,18 +19,6 @@ class EcotoneQuickstart
 
     public function run() : void
     {
-        $this->commandBus->convertAndSend(
-            "product.register",
-            MediaType::APPLICATION_JSON,
-            \json_encode(["productId" => 1, "cost" => 100])
-        );
-
-        $this->commandBus->convertAndSend(
-            "product.changePrice",
-            MediaType::APPLICATION_JSON,
-            \json_encode(["productId" => 1, "cost" => 110])
-        );
-
         echo $this->queryBus->convertAndSend("product.getCost", MediaType::APPLICATION_JSON, \json_encode(["productId" => 1]));
     }
 }
